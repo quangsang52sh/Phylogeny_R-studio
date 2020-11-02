f0d4c3aecdfbe1daebe0af581ec316911528834f
quangsang52sh <51614765+quangsang52sh@users.noreply.github.com>
2020-11-02 17:25:19 +0700

    Add files via upload

---
+++ tree-by-hand.r
@@ -0,0 +1,11 @@
+GTR <- get ("GTR+G+I", env)
+your_model <- eval( GTR, env=env)
+dm <- dist.ml(dat)
+treeNJ <- NJ(dm)
+fit <- pml(treeNJ, data=dat)
+parsimony(treeNJ, dat)
+
+fit1 <- optim.pml(fit, model="GTR", optInv=TRUE, optGamma=TRUE, rearrangement = , control = pml.control(trace = 0))
+bs <- bootstrap.pml(fit1, bs=1000, optNni=TRUE, control = pml.control(trace = 0))
+tree <- plotBS(midpoint(fit1$tree), bs, p = 50, type="p")
+write.tree(tree,"Yourtree.tre")
